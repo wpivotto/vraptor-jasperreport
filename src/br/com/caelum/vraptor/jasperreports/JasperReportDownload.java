7c0a20def6a84dd955893c47c13d866324389db4
wpivotto <wpivotto@gmail.com>
2011-04-06 11:12:13 -0300

    Inserida propriedade para download ou visualizacao do relatorio gerado

--- src/br/com/caelum/vraptor/jasperreports/JasperReportDownload.java
+++ src/br/com/caelum/vraptor/jasperreports/JasperReportDownload.java
@@ -12,16 +12,23 @@ public class JasperReportDownload implements Download {
 	
 	private final Report report;
 	private final ExportFormat format;
+	private final boolean doDownload;
 	
-	public JasperReportDownload(Report report, ExportFormat format){
+	
+	public JasperReportDownload(Report report, ExportFormat format, boolean doDownload){
 		this.report = report;
 		this.format = format;
+		this.doDownload = doDownload;
+	}
+	
+	public JasperReportDownload(Report report, ExportFormat format){
+		this(report, format, true);
 	}
 
 	public void write(HttpServletResponse response) throws IOException {
 		byte[] bytes = new JasperExporter().export(report).to(format);
 		String filename = report.getFileName() + format.getExtension();
-		new ByteArrayDownload(bytes, format.getContentType(), filename).write(response);
+		new ByteArrayDownload(bytes, format.getContentType(), filename, doDownload).write(response);
 	}
 
 }
