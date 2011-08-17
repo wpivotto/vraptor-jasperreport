9b4009a78f1cbd64d8f0ce2e8670064b0cf99978
wpivotto <wpivotto@gmail.com>
2011-08-17 18:05:35 -0300

    PDF Encryption

--- src/br/com/caelum/vraptor/jasperreports/formats/Pdf.java
+++ src/br/com/caelum/vraptor/jasperreports/formats/Pdf.java
@@ -15,6 +15,7 @@ import br.com.caelum.vraptor.jasperreports.ExportFormat;
 public class Pdf implements ExportFormat {
 	
 	private JRExporter exporter;
+	private Integer permissions = 0;
 
 	public Pdf(){
 		this.exporter = new JRPdfExporter();
@@ -36,6 +37,19 @@ public class Pdf implements ExportFormat {
 	
 	public ExportFormat configure(JRExporterParameter parameter, Object value) {
 		exporter.setParameter(parameter, value);
+		return this;
+	}
+	
+	public void encrypt(String password){
+		configure(JRPdfExporterParameter.IS_ENCRYPTED, Boolean.TRUE);
+		configure(JRPdfExporterParameter.IS_128_BIT_KEY, Boolean.TRUE);
+		configure(JRPdfExporterParameter.USER_PASSWORD, password);
+		configure(JRPdfExporterParameter.OWNER_PASSWORD, password);
+	}
+	
+	public Pdf addPermission(Integer permission){
+		this.permissions |= permission; 
+		configure(JRPdfExporterParameter.PERMISSIONS, permissions);
 		return this;
 	}
 
