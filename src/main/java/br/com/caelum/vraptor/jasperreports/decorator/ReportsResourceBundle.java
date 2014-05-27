e831cf1c8bf50974c644db52333ceb1d66b922ea
wpivotto <wpivotto@gmail.com>
2014-05-27 10:51:32 -0300

    Fix ReportsResourceBundle

--- src/main/java/br/com/caelum/vraptor/jasperreports/decorator/ReportsResourceBundle.java
+++ src/main/java/br/com/caelum/vraptor/jasperreports/decorator/ReportsResourceBundle.java
@@ -10,7 +10,9 @@ import java.util.MissingResourceException;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,22 +23,19 @@ import br.com.caelum.vraptor.util.EmptyBundle;
 @RequestScoped
 public class ReportsResourceBundle extends ResourceBundle {
 
-	private ResourceBundle delegate;
-	private Locale locale;
+	@Inject private Locale locale;
+	@Inject private ResourceBundle delegate;
+	@Inject private ReportPathResolver resolver;
 	private static final Logger logger = LoggerFactory.getLogger(ReportsResourceBundle.class);
 
-	public ReportsResourceBundle(Localization localization, ReportPathResolver resolver) {
-		
-		this.locale = localization.getLocale();
-		buildBundle(resolver.getResourceBundleFor(locale));
-	}
-	
 	public Locale getLocale(){
 		return locale;
 	}
 	
-	private void buildBundle(String path){
+	@PostConstruct
+	private void buildBundle(){
 		
+		String path = resolver.getResourceBundleFor(locale);
 		File bundle = new File(path);
 		
 		try {	
